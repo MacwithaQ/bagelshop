73971240f2ff91f4e99f670591834f39b62e1e01
MacwithaQ <maqallaf@gmail.com>
2022-01-16 22:01:27 +0300

    added components folder and made automatic item update using map

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
-import './App.css';
-import products from './products.js'
-import Home from './Home.js'
-import ProductList from './ProductList';
-
+import "./App.css";
+import products from "./products.js";
+import Home from "./components/Home.js";
+import ProductList from "./components/ProductList";
 
 function App() {
   return (
     <div className="App">
       <Home />
-      <ProductList product= {products[1]}/>
-      <ProductList product= {products[0]}/>
+      {products.reverse().map((element) => (
+        <ProductList product={element} />
+      ))}
+      {/* <ProductList product={products[1]} />
+      <ProductList product={products[0]} /> */}
     </div>
-  
-    
   );
 }
 
