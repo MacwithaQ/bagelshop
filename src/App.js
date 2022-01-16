3dbd8e923a8c7593f293c9c5c09a38ee4791812b
MacwithaQ <maqallaf@gmail.com>
2022-01-16 18:03:56 +0300

    completed challenge

--- src/App.js
+++ src/App.js
@@ -4,10 +4,12 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <div className='atTheTop'>
       <h1>The Bagel Shop</h1>
       <h3>Our specialty is freshly bakes bagels that tickle your taste buds!</h3>
+      </div>
       <img src="https://media-cdn.tripadvisor.com/media/photo-s/12/6e/a8/32/doggy-friendly-bagel.jpg" alt="Bagel Shop"></img>
-      
+
     </div>
   );
 }
